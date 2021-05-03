188198b70776ba824fd25598163afb0450873b72
Lindsay Esterman <lindsayesterman@gmail.com>
2021-05-03 00:15:16 -0400

    new landing pics

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
-import openingGlassesModel from "./openingGlassesModel.png";
+import redGlass from "./redGlassesModel.png";
+import manGlass from "./manGlassesModel.png";
+import fadeGlass from "./fadeGlassesModel.png";
 
 export default class LandingPage extends Component {
   render() {
     return (
       <div className="openingimg">
-        <img className="car-top open" alt="model wearing glasses" src={openingGlassesModel}></img>
-      </div>
+        <img className="car-top open" src={manGlass}></img>
+        <img className="dye-top open" src={redGlass}></img>
+        <img className="shoes open" src={fadeGlass}></img>      </div>
     );
   }
 }
