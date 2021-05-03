aac3dc8e2fdd36675dfa37eb5ca7c3082092f46b
Lindsay Esterman <lindsayesterman@gmail.com>
2021-05-03 00:32:21 -0400

    alt prop

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -7,9 +7,10 @@ export default class LandingPage extends Component {
   render() {
     return (
       <div className="openingimg">
-        <img className="car-top open" src={manGlass}></img>
-        <img className="dye-top open" src={redGlass}></img>
-        <img className="shoes open" src={fadeGlass}></img>      </div>
+        <img className="car-top open" src={manGlass} alt="man wearing glasses"></img>
+        <img className="dye-top open" src={redGlass} alt="women wearing red glasses"></img>
+        <img className="shoes open" src={fadeGlass} alt="women wearing glasses"></img>{" "}
+      </div>
     );
   }
 }
